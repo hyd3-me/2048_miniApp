45c5d9534b5b6388c9b905ec61175b393ec0d40a
sedg <hyde.rou@yandex.ru>
2025-09-18 16:12:24 +0300

    add decorators fabric for connect to session

--- app/database.py
+++ app/database.py
@@ -10,6 +10,29 @@ engine = create_async_engine(url=database_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
 
 
+from functools import wraps
+
+def connection(isolation_level=None):
+    def decorator(method):
+        @wraps(method)
+        async def wrapper(*args, **kwargs):
+            async with async_session_maker() as session:
+                try:
+                    # Устанавливаем уровень изоляции, если передан
+                    if isolation_level:
+                        await session.execute(text(f"SET TRANSACTION ISOLATION LEVEL {isolation_level}"))
+                    # Выполняем декорированный метод
+                    return await method(*args, session=session, **kwargs)
+                except Exception as e:
+                    await session.rollback()  # Откатываем сессию при ошибке
+                    raise e  # Поднимаем исключение дальше
+                finally:
+                    await session.close()  # Закрываем сессию
+
+        return wrapper
+    return decorator
+
+
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True  # Абстрактный базовый класс, чтобы избежать создания отдельной таблицы
 
